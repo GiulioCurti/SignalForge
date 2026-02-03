Major code addition and fixes: *Fixes:     - Fixed normalization factors on discrete fourier transform     - Fixed missing documentation on plot_stft()     - Fixed behavior when axes handle is given to function plot_hilbert() *Additions:     - Added two side ft plot on plot_fft()     - Added official computation of the fast kurtogram on get_kurtogram()     - Added Nfft control on plot_spectral_kurtosis()     - Added get_squared_envelope_spectrum()     - Added plot_squared_envelope_spectrum()     - Added _plot_squared_envelope_stft() as an initial test

SignalForge/single_chan_signal.py
@@ -136,7 +136,7 @@ class SingleChanSignal:
     @property # for storage efficiency
     def fft_ts(self):
         """two-sided Fourier coefficients"""   
-        Xts_woShift = self.dt*np.fft.fft(self.x)
+        Xts_woShift = np.fft.fft(self.x)/self.N
         return np.fft.fftshift(Xts_woShift)
     
     @property # for storage efficiency
@@ -148,16 +148,14 @@ class SingleChanSignal:
     @property # for storage efficiency
     def fft_os(self):
         """one-sided Fourier coefficients"""
-        Xts_woShift = self.dt*np.fft.fft(self.x)
-        Nos         = np.ceil((self.N+1)/2)             # number of one-sided Fourier coefficients
-        return Xts_woShift[0:int(Nos)]       
+        Xos = np.fft.rfft(self.x)*2/self.N
+        return Xos       
      
     @property # for storage efficiency
     def f_fft_os(self):
         """one-sided frequency vector"""
-        frq_woShift = np.fft.fftfreq(self.N, self.dt)
-        Nos         = np.ceil((self.N+1)/2)             # number of one-sided Fourier coefficients
-        return np.abs(frq_woShift[0:int(Nos)])
+        frq_os = np.fft.rfftfreq(self.N, self.dt)
+        return frq_os
     
 
     def get_psd(self,df = 0.5,noverlap=None):
@@ -328,6 +326,27 @@ class SingleChanSignal:
         amplitude_envelope = np.abs(analytic_signal)
         instantaneous_phase = np.unwrap(np.angle(analytic_signal))
         return analytic_signal, amplitude_envelope, instantaneous_phase
+
+    def get_squared_envelope_spectrum(self):
+        """
+        Plot the Squared Envelope Spectrum (SES) of the signal.
+
+        Parameters
+        ----------
+
+
+        Returns
+        -------
+        freq : ndarray
+            Frequency array of the SES
+        ses : ndarray
+            SES array 
+        """
+        _, absolute_envelope, _ = self.get_hilbert()
+        se = absolute_envelope**2
+        ses =  np.abs(np.fft.rfft(se)/self.N)**2
+        freq = self.f_fft_os
+        return ses, freq
     
     def get_wavelet_transform(self, wavetype = 'morl', **kwargs):
         """
@@ -400,7 +419,7 @@ class SingleChanSignal:
         f_norm, SK = get_welch_spectral_kurtosis(self.x, Nfft, noverlap)
         return f_norm*self.fs, SK
 
-    def get_kurtogram(self, n_bins:tuple = (10,100,10), _plot_call = False):
+    def get_kurtogram(self, n_bins: tuple|int = None, method = 'fast', _plot_call = False):
         """
         Calculate the kurtogram of the signal.
 
@@ -418,22 +437,52 @@ class SingleChanSignal:
         """
         print(f'Calculating Kurtogram')
         np_freqs = self.N//2
-        try:
-            n_divisions = np.arange(*n_bins)
-        except:
-            raise ValueError('n_bins argument does not meet requirement. Try with a tuple with np.arange() format "(start, finish, step)"')
-        max_Nfft = np_freqs//np.min(n_divisions)
-        ref_fvec = np.linspace(0,1,max_Nfft)*self.fs/2
-        kurtogram = np.zeros((max_Nfft,len(n_divisions)))
-        for i, divisions in tqdm(enumerate(n_divisions)):
-            Nfft = np_freqs//(divisions)
-            fvec, spectral_kurtosis = get_welch_spectral_kurtosis(self.x,Nfft = Nfft, noverlap = round(3/8*Nfft))
-            kurtogram[:,i] = step_interp(ref_fvec, fvec*self.fs, spectral_kurtosis)
-        if _plot_call:
-            return kurtogram, n_divisions, max_Nfft
-        else:
-            return kurtogram
         
+        if method.lower() == 'fast':
+            if n_bins is None:
+                n_bins = 7
+            elif isinstance(n_bins, int) is False:
+                raise ValueError('n_bins argument does not meet requirements. Format it as an integer')         
+            kurtogram, Level_w, freq_w, fc, bandwidth ,max_kurt,level_max,c = fast_kurtogram(self.x, self.fs, n_bins)
+            if _plot_call:
+                idx_max = np.argmax(kurtogram)
+                I, J = np.unravel_index(idx_max, kurtogram.shape)  # 0-based indices
+                M = kurtogram[I, J]
+                max_kurt = np.round(10*M)/10
+                max_level = np.floor(10*Level_w[I])/10
+                fi = J / (3 * 2**(n_bins + 1))          # normalized frequency
+                fi = fi + 2**(-2 - Level_w[I])
+                bw = self.fs * 2**(-(Level_w[I] + 1))
+                fc = self.fs * fi
+                return kurtogram, np.flip(np.linspace(0,n_bins, 2*n_bins)), np.shape(kurtogram)[1], max_kurt, max_level, bw, fc
+            else:
+                return kurtogram, c
+                        
+        elif method.lower() == 'banded':
+            if n_bins is None:
+                n_divisions = 2**np.arange(4,12,1)
+            else:
+                try:
+                    n_divisions = 2**np.arange(*n_bins)
+                except:
+                    raise ValueError('n_bins argument does not meet requirements. Try with a tuple with np.arange() format "(start, finish, step)"')
+            # max_Nfft = np_freqs//np.min(n_divisions)
+            max_Nfft = np.max(n_divisions)
+            ref_fvec = np.linspace(0,1,max_Nfft)*self.fs/2
+            kurtogram = np.zeros((max_Nfft,len(n_divisions)))
+            for i, divisions in tqdm(enumerate(n_divisions)):
+                # Nfft = np_freqs//(divisions)
+                Nfft = divisions
+                fvec, spectral_kurtosis = get_welch_spectral_kurtosis(self.x,Nfft = Nfft, noverlap = round(3/8*Nfft))
+                kurtogram[:,i] = step_interp(ref_fvec, fvec*self.fs, spectral_kurtosis)
+            if _plot_call:
+                return kurtogram, n_divisions, max_Nfft
+            else:
+                return kurtogram
+            
+        else:
+            raise ValueError('Method not implemented. Try with "fast" or "banded"')
+            
     def get_stat_history(self, winsize:int, olap:int = 0, idx_type = 'rms'):
         """
         Compute a statistical index over a moving window.
@@ -595,6 +644,7 @@ class SingleChanSignal:
     def plot_fft(
         self,
         ax = None, 
+        two_sided: bool = False,
         xlims: list = None, 
         ylims: list = None
         ):
@@ -628,7 +678,10 @@ class SingleChanSignal:
         label = f"$FT_{{{self.var}}}$: rms = {s_rms:.2f} [{self.unit}]"
         
         ax0 = ax_array[0]
-        ax0.plot(self.f_fft_os, np.abs(self.fft_os), 'k', label=label)
+        if two_sided:
+            ax0.plot(self.f_fft_ts, np.abs(self.fft_ts), 'k', label=label)
+        else:
+            ax0.plot(self.f_fft_os, np.abs(self.fft_os), 'k', label=label)
         ax0.set_ylabel(f"$FT_{{{self.var}}}$ [{self.unit}]")
         ax0.legend()   
         ax0.grid(True, which='both')
@@ -636,7 +689,10 @@ class SingleChanSignal:
         ax0.set_title(f'Fourier transform {self.name}')
         
         if is_array:
-            ax_array[1].plot(self.f_fft_os, np.angle(self.fft_os), 'k', label=label)
+            if two_sided:
+                ax_array[1].plot(self.f_fft_ts, np.angle(self.fft_ts), 'k', label=label)
+            else:
+                ax_array[1].plot(self.f_fft_os, np.angle(self.fft_os), 'k', label=label)
             ax_array[1].set_ylabel(f"$\\phi_{{{self.var}}}$ [rad]")
             ax_array[1].grid(True, which='both')
             ax_array[1].minorticks_on()
@@ -772,7 +828,13 @@ class SingleChanSignal:
             The number of data points per segment for the STFT.
         hop : int, optional 
             The number of data points between segments.
-
+        flims : list, optional
+            The limits of the frequencies shown of the plot 
+        fig : optional 
+            Matplotlib figure handle
+        ax : optional
+            Matplotlib axis handle
+            
         Returns
         -------
         ax : matplotlib.axes.Axes
@@ -804,6 +866,7 @@ class SingleChanSignal:
     def plot_spectral_kurtosis(
         self,
         ax = None, 
+        Nfft:int = None,
         n_bins:int = None, 
         fl : int = None, 
         fu : int = None, 
@@ -816,6 +879,8 @@ class SingleChanSignal:
         ----------
         ax : matplotlib.axes.Axes, optional 
             The axes to plot on. If None, a new figure and axes are created.
+        Nfft : int, optional
+            Number of opints used to calculate the spectral kurtosis with the fast approach
         n_bins : int, optional 
             The number of bins for the spectral kurtosis calculation.
         fl : int, optional 
@@ -832,16 +897,18 @@ class SingleChanSignal:
         """
         if n_bins and fl and fu: 
             fvec, spectral_kurtosis = get_banded_spectral_kurtosis(self.x,self.dt,n_bins, fl, fu)
+        elif Nfft:
+            fvec, spectral_kurtosis = get_welch_spectral_kurtosis(self.x,Nfft = Nfft, noverlap = Nfft//4)
         else: 
             fvec, spectral_kurtosis = get_welch_spectral_kurtosis(self.x,Nfft = 2**10, noverlap = 2**8)
             
-        print(f'Plotting spectral kurtosis from {fvec[0]:.1f} Hz to {fvec[-1]*self.fs/2:.1f} Hz')
+        print(f'Plotting spectral kurtosis from {fvec[0]:.1f} Hz to {fvec[-1]*self.fs:.1f} Hz')
         if ax is None:
             _, ax = plt.subplots(figsize=(10, 4))
         s_kur = self.central_moments['kurtosis']
         glob = 'global'
         label = f"$SK_{self.var}$(f): $kurt_{{{glob}}}$ = {s_kur:.2f} [-]"
-        ax.step(fvec*self.fs/2, spectral_kurtosis,'k',label=label, where = 'mid')
+        ax.step(fvec*self.fs, spectral_kurtosis,'k',label=label, where = 'mid')
         ax.set_xlabel("frequency [Hz]")
         ax.set_ylabel(f"$SK_{self.var}$ [-]")
         ax.legend()   
@@ -857,8 +924,10 @@ class SingleChanSignal:
     def plot_kurtogram(
         self,
         ax = None, 
-        n_bins:tuple = (10,100,10), 
-        ylims:list = None
+        n_bins: tuple|int = None, 
+        method: str = 'fast',
+        ylims:list = None,
+        xlims:list = None,
         ):
         """
         Plot the kurtogram of the signal.
@@ -877,22 +946,41 @@ class SingleChanSignal:
         ax : matplotlib.axes.Axes 
             The axes with the plotted kurtogram.
         """      
-        kurtogram, n_divisions, max_Nfft = self.get_kurtogram(n_bins = n_bins, _plot_call = True)
-        print(f'Plotting Kurtogram')
         if ax is None:
             fig, ax = plt.subplots(figsize=(10, 4), tight_layout=True)
-        freq_edges = np.linspace(0, self.fs / 2, max_Nfft + 1)
-        band_edges = np.append(n_divisions, n_divisions[-1] + (n_divisions[1] - n_divisions[0]))
-        FE, BE = np.meshgrid(np.flip(band_edges), freq_edges)
+        print(f'Plotting Kurtogram')
+        
+        if method == 'fast':
+            kurtogram, band_edges, max_Nfft, max_kurt, max_level, bw, fc = self.get_kurtogram(n_bins = n_bins, method = method, _plot_call = True)
+            title_str = (f'Kurtogram {self.name}\n'
+                r"$K_{max}$="
+                f"{max_kurt} @ level {max_level}, "
+                f"Bw={bw:.2f} Hz, $f_c$={fc:.2f} Hz"
+                )
+            kurtogram = kurtogram.transpose()
+            kurtogram = np.flip(kurtogram, axis = 1)
+        elif method == 'banded':    
+            kurtogram, n_divisions, max_Nfft = self.get_kurtogram(n_bins = n_bins, method = method, _plot_call = True)
+            # band_edges = np.log2(np.flip(np.append(n_divisions, n_divisions[-1] + 
+            #                        (n_divisions[1] - n_divisions[0]))))
+            band_edges = np.log2(np.flip(n_divisions))
+            kurtogram = np.flip(kurtogram, axis = 1)
+            title_str = f'Kurtogram {self.name}'
+            
+        freq_edges = np.linspace(0, self.fs / 2, max_Nfft)
+        FE, BE = np.meshgrid(band_edges, freq_edges)
         mesh = ax.pcolormesh(BE, FE, kurtogram, shading='auto')
         plt.gca().invert_yaxis()
         cbar = fig.colorbar(mesh, ax = ax)
         cbar.ax.set_ylabel('Spectral kurtosis')
         ax.set_xlabel('Frequency (Hz)')
-        ax.set_ylabel('Number of bands')
-        ax.set_title(f'Kurtogram {self.name}')
+        ax.set_ylabel('Level [log2(Window length)]')
+        ax.set_title(title_str)
         if ylims is not None:
             ax.set_ylim(ylims)
+        if xlims is not None:
+            ax.set_xlim(xlims)
+        plt.tight_layout()
         plt.show(block=False)
         return ax    
     
@@ -965,61 +1053,134 @@ class SingleChanSignal:
         ax : matplotlib.axes.Axes
             The axes with the plotted Hilbert transform.
         """
+        
         _, amplitude_envelope, instantaneous_phase = self.get_hilbert()
         print(f'Plotting Hilbert transform')
         instantaneous_frequency = np.diff(instantaneous_phase) / (2*np.pi) * self.fs
         time = self.t
         if ax is None:
             _, ax = plt.subplots(nrows=2, figsize=(10, 4), sharex='all', tight_layout=True)
+        ax_array = np.atleast_1d(ax)
+        is_array = len(ax_array) > 1
         ax[0].set_title(f"Hilbert transform {self.name}")
-        ax[0].set_ylabel(f"Amplitude [{self.unit}]")
+        ax[0].set_ylabel(f"SES [{self.unit}]")
         ax[0].plot(time, self.x, 'k', label=f'Signal')
         ax[0].plot(time, amplitude_envelope, 'r', label='Envelope')
         ax[0].legend()
         ax[0].grid(True,which = 'both')
         ax[0].minorticks_on() 
-        ax[1].set(xlabel="Time [s]", ylabel="Frequency [Hz]")
-        ax[1].plot(time[1:], abs(instantaneous_frequency), 'k', label = 'Instantaneous frequency')
-        ax[1].legend()
-        ax[1].grid(True,which = 'both')
-        ax[1].minorticks_on() 
+        if is_array:
+            ax[1].set(xlabel="Time [s]", ylabel="Frequency [Hz]")
+            ax[1].plot(time[1:], abs(instantaneous_frequency), 'k', label = 'Instantaneous frequency')
+            ax[1].legend()
+            ax[1].grid(True,which = 'both')
+            ax[1].minorticks_on() 
         plt.show(block=False)
         return ax
     
-    def _plot_hilbert_spectrum(self, ax = None): #! WIP
+    def plot_squared_envelope_spectrum(
+        self, 
+        ax = None, 
+        xlims:list = None,
+        ylims:list|str = None,
+        log:str = None
+        ):
         """
-        Plot the Hilbert spectrum of the signal, showing the amplitude and phase.
+        Plot the Squared Envelope Spectrum (SES) of the signal.
 
         Parameters
         ----------
         ax : matplotlib.axes.Axes, optional
             The axes to plot on. If None, a new figure and axes are created.
+        xlims : list, optional 
+            The limits for the x-axis.
+        ylims : list, optional 
+            The limits for the y-axis. Set 'auto' for automatic scaling on f>0
+        log : str, optional 
+            Specifies the logarithmic scale for x, y, or both axes.
 
         Returns
         -------
         ax : matplotlib.axes.Axes
-            The axes with the plotted Hilbert spectrum.
+            The axes with the plotted squared envelope spectrum.
         """
-        _, amplitude_envelope, instantaneous_phase = self.get_hilbert()
-        instantaneous_frequency = np.diff(instantaneous_phase,prepend=0) / (2*np.pi) * self.fs
-        h_spectrum =  np.fft.rfft(amplitude_envelope)/self.fs
-        freq = self.f_fft_os
+        ses, freq = self.get_squared_envelope_spectrum()
         if ax is None:
-            _, ax = plt.subplots(nrows=2, figsize=(10, 4), sharex='all', tight_layout=True)
-        ax[0].set_title(f"Hilbert spectrum {self.name}")
-        ax[0].set_ylabel(f"Amplitude [{self.unit}]")
-        ax[0].plot(freq, abs(h_spectrum), 'k')
-        ax[0].legend()
-        ax[0].grid(True,which = 'both')
-        ax[0].minorticks_on() 
-        ax[1].set(xlabel="Frequency [Hz]", ylabel="Phase [rad]")
-        ax[1].plot(freq, np.angle(h_spectrum), 'k')
-        # ax[1].legend()
-        ax[1].grid(True,which = 'both')
-        ax[1].minorticks_on() 
+            _, ax = plt.subplots(figsize=(10, 4), tight_layout=True)
+        ax.set_title(f"Squared Envelope Spectrum {self.name}")
+        ax.set_ylabel(f"Amplitude [({self.unit})$^4/Hz$]")
+        ax.plot(freq, abs(ses), 'k')
+        ax.legend()
+        ax.grid(True,which = 'both')
+        ax.minorticks_on() 
+        if ylims is not None:
+            if ylims == 'auto':
+                ax.set_ylim([0, 1.5*max(abs(ses[2:]))])
+            else:
+                ax.set_ylim(ylims)
+        if xlims is not None:
+            ax.set_xlim(xlims)
+        if log == "x":
+            ax.set_xscale("log")
+        elif log == "y":
+            ax.set_yscale("log")
+        elif log == "both":
+            ax.set_xscale("log")
+            ax.set_yscale("log") 
         plt.show(block=False)
         return ax
+    
+    def plot_squared_envelope_stft(
+        self, 
+        fig = None, 
+        ax = None, 
+        flims:list = None
+        ): #!WIP
+        """
+        Plot the Squared Envelope Spectrum (SES) of the signal.
+
+        Parameters
+        ----------
+        fig : matplotlib.axes.Figure, optional
+            The axes to plot on. If None, a new figure and axes are created.
+        ax : matplotlib.axes.Axes, optional
+            The axes to plot on. If None, a new figure and axes are created.
+        flims : list, optional
+            Limits the frequency range on the plot.
+            
+        Returns
+        -------
+        ax : matplotlib.axes.Axes
+            The axes with the plotted squared envelope spectrum.
+        """
+        _, absolute_envelope, _ = self.get_hilbert()
+        se = absolute_envelope**2
+        nperseg:int = 2**10
+        hop = 2**5
+        w = sp.signal.windows.hann(nperseg)  
+        SFT = sp.signal.ShortTimeFFT(w, hop=hop, fs=self.fs, mfft=nperseg*2, scale_to='magnitude')
+        Sx = SFT.stft(se)
+        if ax is None and fig is None:
+            fig, ax = plt.subplots(figsize=(10, 4), sharex='all', tight_layout=True)
         
+        t_lo, t_hi = SFT.extent(self.N)[:2]  # time range of plot
+        ax.set_title(rf"SE Short time Fourier transform {self.name}")
+        ax.set(xlabel=f"Time [s] ({SFT.p_num(self.N)} slices, " +
+                    rf"$\Delta t = {SFT.delta_t:.3f}\,$s)",
+                ylabel=f"Frequency [Hz] ({SFT.f_pts} bins, " +
+                    rf"$\Delta f = {SFT.delta_f:.3f}\,$Hz)",
+                xlim=(t_lo, t_hi))
+
+        im1 = ax.imshow(abs(Sx), origin='lower', aspect='auto',
+                        extent=SFT.extent(self.N))
+        fig.colorbar(im1, label=f"Magnitude $|S_{self.var}(t, f)|$ [({self.unit})$^2$]")
+        if flims:
+            plt.ylim(flims)
+        plt.show(block=False)
+
+        return ax
+        
+    
     def plot_scalogram(self, ax = None, wavetype = 'morl', **kwargs):
         """
         Plot the scalogram of the signal using wavelet transformation.

SignalForge/utils.py
@@ -379,14 +379,14 @@ def get_banded_spectral_kurtosis(
     fper = np.linspace(0, f_ny, len(Xos_full))
     N = len(Xts_full_woShift)
     binsize = (fu-fl) / n_bins
-    fvec = np.arange(n_bins) * binsize + fl + binsize/ 2
+    fvec = (np.arange(n_bins) * binsize + fl + binsize/ 2)*dt
     SK = np.zeros(n_bins)
     print('Evaluating kurtosis on frequency bands:')
     for i in tqdm(np.arange(n_bins)):
         Xos = np.zeros(len(Xos_full), dtype=np.complex128)
         Xos[(fper>=(fl+binsize*i)) & (fper<=(fl+binsize*(i+1)))] = Xos_full[(fper>=(fl+binsize*i)) & (fper<=(fl+binsize*(i+1)))]
         x = np.fft.irfft(Xos,n = N)/dt
-        SK[i] = sp.stats.kurtosis(x)+3    
+        SK[i] = sp.stats.kurtosis(x)    
     return fvec, SK
 
 def get_welch_spectral_kurtosis(x:np.ndarray, Nfft:int, noverlap:int = None):
@@ -448,6 +448,471 @@ def get_welch_spectral_kurtosis(x:np.ndarray, Nfft:int, noverlap:int = None):
     f_norm = np.arange(Nfft) / Nfft
 
     return f_norm[:Nfft//2], SK[:Nfft//2]
+
+def fast_kurtogram(x, fs, nlevel=7,plot=False):
+    '''
+    Fast Kurtogram computation
+    ===========================================================================
+    
+    Input parameters:
+    --------------------------
+    x       : input signal (1D array)
+    nlevel  : number of decomposition levels (Maximum number of decomposition levels is log2(length(x)), but it is recommended to stay by a factor 1/8 below this.)
+    fs      : sampling frequency of signal x (default is Fs = 1)
+    plot    : boolean to enable/disable plotting of results (default is False)
+    ===========================================================================
+    Output parameters:
+    --------------------------
+    Output parameters:
+    --------------------------
+    Kwav        : 2D ndarray of shape (2 * nlevel, 3 * 2**nlevel)
+                  Kurtosis map (Fast Kurtogram). Each entry Kwav[i, j]
+                  is the envelope kurtosis of the sub-band corresponding
+                  to level Level_w[i] and center frequency freq_w[j].
+
+    Level_w     : 1D ndarray of length 2 * nlevel
+                  Effective decomposition levels associated with each row
+                  of Kwav. It contains both binary and ternary levels,
+                  ordered as in the original MATLAB implementation
+                  (top row = level 0).
+
+    freq_w      : 1D ndarray of length 3 * 2**nlevel
+                  Center frequencies [Hz] of each sub-band along the
+                  horizontal axis (columns) of Kwav.
+
+    fc          : float
+                  Optimal center frequency [Hz] corresponding to the
+                  maximum kurtosis in Kwav (i.e. frequency of the most
+                  impulsive band).
+
+    bandwidth   : float
+                  Bandwidth [Hz] of the optimal sub-band associated with fc.
+
+    max_kurt    : float
+                  Maximum kurtosis value found in Kwav.
+
+    level_max   : float
+                  Effective decomposition level (in Level_w) at which the
+                  maximum kurtosis max_kurt occurs.
+
+    c           : 1D ndarray
+                  Signal filtered in the optimal sub-band (center frequency fc
+                  and bandwidth bandwidth). This is the band-limited signal
+                  typically used for further analysis (e.g., envelope analysis
+                  and envelope spectrum for fault detection).
+    '''
+    # % Fast_Kurtogram(x,nlevel,Fs)
+    # % Computes the fast kurtogram of signal x up to level 'nlevel' via a fast decimated filterbank tree.
+    # % Maximum number of decomposition levels is log2(length(x)), but it is 
+    # % recommended to stay by a factor 1/8 below this.
+    # % Fs = sampling frequency of signal x (default is Fs = 1)
+    # %
+    # % --------------------------
+    # % Reference: J. Antoni, Fast Computation of the Kurtogram for the Detection of Transient Faults, 
+    # % Mechanical Systems and Signal Processing, Volume 21, Issue 1, 2007, pp.108-124.
+    # % --------------------------
+    # % Author: J. Antoni
+    # % Last Revision: 12-2014
+    # % Last Translation: 12-2025 (Python 3.11.0)
+    # % --------------------------
+    
+    firwin = sp.signal.firwin
+    lfilter = sp.signal.lfilter
+    
+    # INTERNAL FUNCTIONS
+    def _kurt(this_x,opt):
+    
+        eps = 2.2204e-16
+
+        if opt.lower() == 'kurt2':
+            if np.all(this_x == 0):
+                K = 0
+                return K
+            this_x -= np.mean(this_x)
+            
+            E = np.mean(np.abs(this_x)**2)
+            
+            if E < eps:
+                K = 0
+                return K
+            K = np.mean(np.abs(this_x)**4) / E**2
+            
+            if np.all(np.isreal(this_x)):
+                K -= 3
+            else:
+                K -= 2
+        elif opt.lower() == 'kurt1':
+            if np.all(this_x == 0):
+                K = 0
+                return K
+            this_x = this_x - np.mean(this_x)
+            E = np.mean(np.abs(this_x))
+            
+            if E < eps:
+                K = 0
+                return K
+            
+            K = np.mean(np.abs(this_x)**2) / E**2
+            
+            if np.all(np.isreal(this_x)):
+                K -= 1.57
+            else:
+                K -= 1.27        
+                
+        
+        return K
+
+
+    def _K_wpQ(x,h,g,h1,h2,h3,nlevel,opt,level=None):
+        '''
+        Computes the kurtosis K of the complete "binary-ternary" wavelet packet transform w of signal x, 
+        up to nlevel, using the lowpass and highpass filters h and g, respectively. 
+        The values in K are sorted according to the frequency decomposition.
+        '''
+        
+        if level == None:
+            level = nlevel
+            
+        x = x.flatten()
+        L = np.floor(np.log2(x.size))
+        x = np.atleast_2d(x).T
+        
+        KD,KQ = _K_wpQ_local(x,h,g,h1,h2,h3,nlevel,opt,level)
+        
+        K = np.zeros((2 * nlevel,3 * 2**nlevel))
+        
+        K[0,:] = KD[0,:]
+        
+        for i in np.arange(1,nlevel):
+            K[2*i-1,:] = KD[i,:]
+            K[2*i,:] = KQ[i-1,:]
+        
+        K[2*nlevel-1,:] = KD[nlevel,:]
+        
+
+        return K
+        
+    def _K_wpQ_local(x,h,g,h1,h2,h3,nlevel,opt,level):
+        
+        
+        
+        a,d = _DBFB(x,h,g)
+        
+        N = np.amax(a.shape)
+        
+        d = d * (-1)**(np.atleast_2d(np.arange(1,N+1)).T)
+
+        Lh = np.amax(h.shape)
+        Lg = np.amax(g.shape)
+            
+        K1 = _kurt(a[Lh-1:],opt)
+        K2 = _kurt(d[Lg-1:],opt)
+        
+        if level > 2:
+            a1,a2,a3 = _TBFB(a,h1,h2,h3)
+            d1,d2,d3 = _TBFB(d,h1,h2,h3)
+            
+            Ka1 = _kurt(a1[Lh-1:],opt)
+            Ka2 = _kurt(a2[Lh-1:],opt)
+            Ka3 = _kurt(a3[Lh-1:],opt)
+            Kd1 = _kurt(d1[Lh-1:],opt)
+            Kd2 = _kurt(d2[Lh-1:],opt)
+            Kd3 = _kurt(d3[Lh-1:],opt)
+            
+        else:
+            Ka1 = 0
+            Ka2 = 0
+            Ka3 = 0
+            Kd1 = 0
+            Kd2 = 0
+            Kd3 = 0
+        
+        if level == 1:
+            K = np.concatenate((K1 * np.ones(3),K2 * np.ones(3)))
+    #         print(K.shape)
+            KQ = np.array([Ka1,Ka2,Ka3,Kd1,Kd2,Kd3])
+
+        if level > 1:
+            
+            Ka,KaQ = _K_wpQ_local(a,h,g,h1,h2,h3,nlevel,opt,level-1)
+            Kd,KdQ = _K_wpQ_local(d,h,g,h1,h2,h3,nlevel,opt,level-1)
+            
+
+            K1 *= np.ones(np.amax(Ka.shape))
+            K2 *= np.ones(np.amax(Kd.shape))
+            
+            
+            K = np.vstack((np.concatenate([K1,K2]),
+                        np.hstack((Ka,Kd))))
+            
+            
+            Long = int(2/6 * np.amax(KaQ.shape))
+            Ka1 *= np.ones(Long)
+            Ka2 *= np.ones(Long)
+            Ka3 *= np.ones(Long)
+            Kd1 *= np.ones(Long)
+            Kd2 *= np.ones(Long)
+            Kd3 *= np.ones(Long)
+            
+            KQ = np.vstack((np.concatenate([Ka1,Ka2,Ka3,Kd1,Kd2,Kd3]),
+                            np.hstack((KaQ,KdQ))))
+            
+            
+
+        if level == nlevel:
+            
+            K1 = _kurt(x,opt)
+            
+            K = np.vstack((K1 * np.ones(np.amax(K.shape)),K))
+            
+            a1,a2,a3 = _TBFB(x,h1,h2,h3)
+            
+            Ka1 = _kurt(a1[Lh-1:],opt)
+            Ka2 = _kurt(a2[Lh-1:],opt)
+            Ka3 = _kurt(a3[Lh-1:],opt)
+            
+            Long = int(1/3 * np.amax(KQ.shape))
+            
+            Ka1 *= np.ones(Long)
+            Ka2 *= np.ones(Long)
+            Ka3 *= np.ones(Long)
+
+            
+            KQ = np.vstack((np.concatenate([Ka1,Ka2,Ka3]),
+                            KQ[:-2,:]))
+
+        return K,KQ
+        
+            
+        
+    def _TBFB(x,h1,h2,h3):
+        
+        N = x.flatten().size
+        
+        a1 = lfilter(h1,1,x.flatten())
+        a1 = a1[2:N:3]
+        a1 = np.atleast_2d(a1).T
+        
+        a2 = lfilter(h2,1,x.flatten())
+        a2 = a2[2:N:3]
+        a2 = np.atleast_2d(a2).T
+        
+        a3 = lfilter(h3,1,x.flatten())
+        a3 = a3[2:N:3]
+        a3 = np.atleast_2d(a3).T
+        
+        return a1,a2,a3
+        
+    def _DBFB(x,h,g):
+        
+        N = x.flatten().size
+        
+        a = lfilter(h,1,x.flatten())
+        a = a[1:N:2]
+        a = np.atleast_2d(a).T
+        
+        d = lfilter(g,1,x.flatten())
+        
+        d = d[1:N:2]
+        
+        d = np.atleast_2d(d).T
+        
+        return a,d
+
+    def binary(i,k):
+        
+        k = int(k)
+        
+        if i > 2**k:
+            raise ValueError('i must be such that i < 2^k')
+        
+        a = np.zeros(k)
+        
+        temp = i
+        
+        for l in np.arange(k)[::-1]:
+            a[-(l+1)] = np.fix(temp / 2**l)
+            temp -= a[-(l+1)] * 2 ** l
+        
+        return a
+
+
+    def find_wav_kurt(x,h,g,h1,h2,h3,Sc,Fr,opt,Fs):
+        level = np.fix(Sc) + (np.remainder(Sc,1)>=0.5) * (np.log2(3)-1)
+        
+        Bw = 2**(-level - 1)
+        freq_w = np.arange(2**level) / (2**(level+1)) + Bw/2
+        J = np.argmin(np.abs(freq_w - Fr))
+        fc = freq_w[J]
+        i = np.round((fc/Bw - 1/2))
+        
+        
+        
+        if np.remainder(level,1) == 0:
+            
+            acoeff = binary(i,level)
+            bcoeff = np.array([])
+            temp_level = level
+        
+        else:
+            
+            i2 = np.fix(i/3)
+            temp_level = np.fix(level) - 1
+            acoeff = binary(i2,temp_level)
+            bcoeff = i - i2 * 3
+        
+        acoeff = acoeff[::-1]
+
+        c = K_wpQ_filt(x,h,g,h1,h2,h3,acoeff,bcoeff,temp_level)
+        
+        kx = _kurt(c,opt)
+        
+        sig = np.median(np.abs(c)) / np.sqrt(np.pi / 2)
+        
+        threshold = sig * np.sqrt((-2*1**2) * np.log(1 - 0.999))
+        
+        return c, Bw, fc, i
+        
+    def K_wpQ_filt(x,h,g,h1,h2,h3,acoeff,bcoeff,level=None):
+        
+        nlevel = acoeff.size
+        
+        L = np.floor(np.log2(np.amax(x.shape)))
+        
+        if level == None:
+            if nlevel >= L:
+                raise ValueError('nlevel must be smaller')
+            
+            level = nlevel
+        
+        x = np.atleast_2d(x.flatten()).T
+
+        if nlevel == 0:
+            if bcoeff.size == 0:
+                c = x
+            else:
+                c1, c2, c3 = _TBFB(x,h1,h2,h3)
+                
+                if bcoeff == 0:
+                    c = c1[h1.size - 1:]
+                elif bcoeff == 1:
+                    c = c2[h2.size - 1:]
+                elif bcoeff == 2:
+                    c = c3[h3.size - 1:]
+            
+        else:
+            
+            c = K_wpQ_filt_local(x,h,g,h1,h2,h3,acoeff,bcoeff,level)
+
+        
+        return c
+
+    def K_wpQ_filt_local(x,h,g,h1,h2,h3,acoeff,bcoeff,level):
+        
+        a,d = _DBFB(x,h,g)
+        
+        N = a.size
+        
+        level = int(level)
+        
+        d = d*np.array([(-1)**(np.arange(1,N+1))]).T
+        
+        if level == 1:
+            if bcoeff.size == 0:
+                if acoeff[level-1] == 0:
+                    c = a[h.size-1:]
+                else:
+                    c = d[g.size-1:]
+            else:
+                if acoeff[level-1] == 0:
+                    c1,c2,c3 = _TBFB(a,h1,h2,h3)
+                else:
+                    c1,c2,c3 = _TBFB(d,h1,h2,h3)
+                
+                if bcoeff == 0:
+                    c = c1[h1.size - 1:]
+                elif bcoeff == 1:
+                    c = c2[h2.size - 1:]
+                elif bcoeff == 2:
+                    c = c3[h3.size - 1:]
+        
+        if level > 1:
+            if acoeff[level-1] == 0:
+                c = K_wpQ_filt_local(a,h,g,h1,h2,h3,acoeff,bcoeff,level-1)
+            else:
+                c = K_wpQ_filt_local(d,h,g,h1,h2,h3,acoeff,bcoeff,level-1)
+                
+        return c
+        
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # % CHECK INPUT VALUES
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    N = x.flatten().size
+    N2 = np.log2(N) - 7
+
+    if nlevel > N2:
+        raise ValueError('Please enter a smaller number of decomposition levels')
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # % FAST COMPUTATION OF THE KURTOGRAM (by means of wavelet packets or STFT)
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    x = x - np.mean(x)
+
+    # Binary (2-band) analytic filters / Analytic generating filters :
+    Nfir = 16
+    fc0 = 0.4
+    # a short filter is just good enough!
+
+    h = firwin(Nfir+1, fc0) * np.exp(2j * np.pi * np.arange(Nfir+1) * 0.125)
+    n = np.arange(2, Nfir+2) 
+
+    # High-pass filter g (Antoni formulation)
+    g = h[(1-n) % Nfir] * (-1.)**(1-n) # why not -1j? and why 1-n?
+
+    # Ternary (3-band) refinement filters
+    Nfir3 = int(np.fix(3/2 * Nfir))
+# 
+    h1 = firwin(Nfir3+1, 2/3 * fc0) * np.exp(2j * np.pi * np.arange(0, Nfir3+1) * (0.25/3))
+    h2 = h1 * np.exp(2j * np.pi * np.arange(0, Nfir3+1) / 6)
+    h3 = h1 * np.exp(2j * np.pi * np.arange(0, Nfir3+1) / 3)
+
+    # Full binary+ternary wavelet packet kurtosis
+    Kwav = _K_wpQ(x, h, g, h1, h2, h3, nlevel, 'kurt2')     # kurtosis of the complex envelope
+    Kwav = np.clip(Kwav, 0, np.inf)                         # keep positive values only!
+
+
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # % GRAPHICAL DISPLAY OF RESULTS
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Level axis (same as MATLAB)
+    Level_w = np.arange(1, nlevel+1, dtype=float)
+    Level_w = np.vstack((Level_w, Level_w + np.log2(3)-1)).reshape(-1, order='F')
+    Level_w = np.insert(Level_w, 0, 0.0)[:2*nlevel]
+
+    # Frequency axis
+    freq_w = fs * (np.arange(3*2**nlevel)/(3*2**(nlevel+1)) + 1/(3*2**(2+nlevel)))
+
+    # Find max kurtosis (row-wise max, then global)
+    row_max = Kwav[np.arange(Kwav.shape[0]), np.argmax(Kwav, axis=1)]
+    max_level_index = np.argmax(row_max)
+    max_kurt = row_max[max_level_index]
+    level_max = Level_w[max_level_index]
+
+    # Bandwidth and center frequency from (level_max, J)
+    bandwidth = fs * 2**(-(level_max + 1))
+
+    J = np.argmax(Kwav[max_level_index, :])
+
+    fi = J / (3 * 2**(nlevel + 1))               # normalized
+    fi = fi + 2**(-2 - level_max)                # normalized
+    fc = fs * fi                                 # Hz
+    
+        # Extract optimal band / envelope
+    c, _, _, _ = find_wav_kurt(x, h, g, h1, h2, h3, level_max, fi, 'kurt2', fs)
+    
+    
+    return Kwav, Level_w, freq_w, fc, bandwidth ,max_kurt,level_max,c
+    
 
 def get_stationary_gaussian(fpsd:np.ndarray, psd:np.ndarray, T:float, fs:float = None, seed = None, interp:str = 'lin'):
     """